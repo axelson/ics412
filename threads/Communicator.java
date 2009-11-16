5b751c19032c0fe62f1fcdbb86ca7b90d0c4339b
Robert <robert@ubuntu.ubuntu-domain>
2009-11-15 22:48:54 -1000

    Communicator.java comments

--- threads/Communicator.java
+++ threads/Communicator.java
@@ -14,11 +14,11 @@ public class Communicator {
      * Allocate a new communicator.
      */
     public Communicator() {
-      this.listenerQueue = ThreadedKernel.scheduler.newThreadQueue(true);
-      this.speakerQueue = ThreadedKernel.scheduler.newThreadQueue(true);
-      this.mutex = new Lock();
+	this.listenerQueue = ThreadedKernel.scheduler.newThreadQueue(true);
+	this.speakerQueue = ThreadedKernel.scheduler.newThreadQueue(true);
+	this.mutex = new Lock();
     }
-
+    
     /**
      * Wait for a thread to listen through this communicator, and then transfer
      * <i>word</i> to the listener.
@@ -30,20 +30,28 @@ public class Communicator {
      * @param   word    the integer to transfer.
      */
     public void speak(int word) {
+	//Disable interrupts
 	boolean intStatus = Machine.interrupt().disable();
-	//mutex.acquire();
+	
 	KThread thread = null;
+	
+	//While there is no listener in the listener queue
 	while((thread = listenerQueue.nextThread()) == null){
-	  this.speakerQueue.waitForAccess(KThread.currentThread());
-       	  KThread.sleep();
+	    //Puts speaker in the speaker queue
+	    this.speakerQueue.waitForAccess(KThread.currentThread());
+	    //Puts speaker to sleep
+	    KThread.sleep();
 	}
+       
 	buffer = word;
+	
+	//Wakes up the listener
 	thread.ready();
 	
-	//mutex.release();
+	//Restores interrupts
 	Machine.interrupt().restore(intStatus);
     }
-
+    
     /**
      * Wait for a thread to speak through this communicator, and then return
      * the <i>word</i> that thread passed to <tt>speak()</tt>.
@@ -51,30 +59,38 @@ public class Communicator {
      * @return  the integer transferred.
      */    
     public int listen() {
+	//Disable interrupts
 	boolean intStatus = Machine.interrupt().disable();
 	
 	KThread thread = null;
 	
+	//Gets the first speaker in the speaker queue
 	thread = speakerQueue.nextThread();
+	
+	//Puts the listener in the listener queue
 	this.listenerQueue.waitForAccess(KThread.currentThread());
-
+	
+	//If there is a speaker
 	if(thread != null){
+	    //Wake up that speaker
 	    thread.ready();
 	}
+	
+	//Puts the listener to sleep
 	KThread.sleep();
 	
+	//Restores interrupts and returns the buffer (word)
 	Machine.interrupt().restore(intStatus);
         return buffer;
     }
-
+    
     /**
      * Tests whether this module is working.
      */
     public static void selfTest() {
         CommunicatorTest.runTest();
     }
-  private ThreadQueue listenerQueue;
-  private ThreadQueue speakerQueue;
-  private Lock mutex;
-  private int buffer = 0;
+    private ThreadQueue listenerQueue;
+    private ThreadQueue speakerQueue;
+    private int buffer;
 }
